8a162d5ad91f6805b6b210c2775217aaf0bb7f10
joao <j.batistareis@gmail.com>
2018-09-04 20:17:29 -0300

    working map

--- src/app/satellite-map/satellite-map.component.ts
+++ src/app/satellite-map/satellite-map.component.ts
@@ -5,6 +5,7 @@ import { TLE } from '../tle-list/tle';
 // jquery
 declare var $: any;
 declare var satellite: any;
+declare var L: any;
 
 @Component({
   selector: 'app-satellite-map',
@@ -12,6 +13,8 @@ declare var satellite: any;
   styleUrls: ['./satellite-map.component.css']
 })
 export class SatelliteMapComponent implements OnInit {
+
+  private map: any;
 
   public startDate: string = '';
   public endDate: string = '';
@@ -24,7 +27,21 @@ export class SatelliteMapComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    // setup map
+    this.map = L.map('map', {
+      center: [0, 0],
+      zoom: 1,
+      maxZoom: 3,
+      worldCopyJump: true
+    }).setView([0, 0], 1);
+
+    L.tileLayer('osm/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a></a>',
+    }).addTo(this.map);
+
+    this.map.on('contextmenu', (event) => this.map.panTo([0, 0]));
+  }
 
   setStartDate(date: string) {
 
