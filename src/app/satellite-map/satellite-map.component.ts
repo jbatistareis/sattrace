6b05054e5fc7262e3205df2e47812a4780b85784
joao <j.batistareis@gmail.com>
2018-09-08 09:51:19 -0300

    map refactor, added ability to set start and end of path

--- src/app/satellite-map/satellite-map.component.ts
+++ src/app/satellite-map/satellite-map.component.ts
@@ -19,7 +19,12 @@ export class SatelliteMapComponent implements OnInit {
   private map: any;
   private markerIcon = L.icon({ iconUrl: 'images/icons8-satellite-48.png', iconAnchor: [24, 24] });
   private mapDataList: MapData[] = [];
-  private manualDate: Date = undefined;
+  private now: any = moment();
+
+  public startAt: number = -1;
+  public endAt: number = 1;
+  public manualDateStr: string = undefined;
+  public manualDateDisabled: boolean = true;
 
   constructor(private tleTrackService: TleTrackService) { }
 
@@ -42,12 +47,25 @@ export class SatelliteMapComponent implements OnInit {
     L.control.scale().addTo(this.map);
 
     // updaters
-    setInterval(() => this.updateSatellitePositions(this.manualDate || new Date()), 2000);
-    setInterval(() => this.updatePathPositions(this.manualDate || new Date()), 60000);
+    setInterval(() => {
+      this.now.add(1, 'second');
+      this.manualDateStr = this.now.format('YYYY-MM-DD HH:mm:ss');
+    }, 1000);
+    setInterval(() => this.updateSatellitePositions(), 2000);
+    setInterval(() => this.updatePathPositions(), 60000);
 
     // listeners
     this.tleTrackService.toggleTle.subscribe((tle) => this.toggleMapData(tle));
     this.tleTrackService.updatedTle.subscribe((tle) => this.updateSatelliteTle(tle));
+
+    // TEST
+    this.toggleTle(
+      new TLE(
+        1,
+        'ISS (ZARYA)',
+        '1 25544U 98067A   18251.24259628  .00003015  00000-0  53336-4 0  9998',
+        '2 25544  51.6418 324.8013 0005029 133.9199 326.5031 15.53826959131434',
+        3));
   }
 
   selectedTle(): TLE[] {
@@ -75,12 +93,13 @@ export class SatelliteMapComponent implements OnInit {
 
       this.mapDataList.push(mapData);
 
-      this.updatePathPositions(this.manualDate || new Date());
-      this.updateSatellitePositions(this.manualDate || new Date());
+      this.updatePathPositions();
+      this.updateSatellitePositions();
     }
   }
 
-  setSatellitePosition(mapData: MapData, date: Date) {
+  setSatellitePosition(mapData: MapData) {
+    let date = this.now.toDate();
     let positionAndVelocity = satellite.propagate(mapData.orbitData, date);
     let geodeticCoords = satellite.eciToGeodetic(positionAndVelocity.position, satellite.gstime(date));
 
@@ -91,13 +110,12 @@ export class SatelliteMapComponent implements OnInit {
     this.setSatelliteIformation(mapData);
   }
 
-  setSatellitePath(mapData: MapData, date: Date) {
+  setSatellitePath(mapData: MapData) {
     let finalCoords = [];
-    let coordMoment = moment(date);
-    coordMoment.subtract(1, 'hour');
+    let coordMoment = this.now.clone();
+    coordMoment.subtract(-this.startAt, 'hour');
 
-
-    for (let i = 0; i < 180; i++) {
+    for (let i = 0; i < ((-this.startAt + this.endAt) * 60); i++) {
       coordMoment.add(1, 'minute');
       let coordDate = coordMoment.toDate();
 
@@ -110,12 +128,30 @@ export class SatelliteMapComponent implements OnInit {
     mapData.path.setLatLngs(finalCoords);
   }
 
+  updateSatellitePositions() {
+    for (let i = 0; i < this.mapDataList.length; i++)
+      this.setSatellitePosition(this.mapDataList[i]);
+  }
+
+  updatePathPositions() {
+    for (let i = 0; i < this.mapDataList.length; i++)
+      this.setSatellitePath(this.mapDataList[i]);
+  }
+
+  pathPeriodChange() {
+    this.updatePathPositions();
+  }
+
   setManualDate() {
-    // TODO
+    this.now = moment(this.manualDateStr).toDate();
+
+    this.updatePathPositions();
+    this.updateSatellitePositions();
   }
 
   clearManualDate() {
-    this.manualDate = undefined;
+    this.manualDateDisabled = !this.manualDateDisabled;
+    this.now = moment();
   }
 
   findMapListIndexByTleId(id): number {
@@ -124,16 +160,6 @@ export class SatelliteMapComponent implements OnInit {
 
   updateSatelliteTle(tle: TLE) {
     this.mapDataList[this.findMapListIndexByTleId(tle.id)].orbitData = satellite.twoline2satrec(tle.line1, tle.line2);
-  }
-
-  updateSatellitePositions(date: Date) {
-    for (let i = 0; i < this.mapDataList.length; i++)
-      this.setSatellitePosition(this.mapDataList[i], date);
-  }
-
-  updatePathPositions(date: Date) {
-    for (let i = 0; i < this.mapDataList.length; i++)
-      this.setSatellitePath(this.mapDataList[i], date);
   }
 
   setSatelliteIformation(mapData: MapData) {
